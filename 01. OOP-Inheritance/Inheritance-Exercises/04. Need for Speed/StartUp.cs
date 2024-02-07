5c147fe9946a3336c7d969c45c4be0a836592a82
Galin <galin_3xg@abv.bg>
2024-02-07 18:52:04 +0200

    Add file

---
+++ 01. OOP-Inheritance/Inheritance-Exercises/04. Need for Speed/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using NeedForSpeed;
+using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            FamilyCar familyCar = new FamilyCar(100, 50);
+            familyCar.Drive(10);
+            Console.WriteLine(familyCar.Fuel);
+
+            CrossMotorcycle cross = new CrossMotorcycle(150, 50);
+            cross.Drive(10);
+            Console.WriteLine(cross.Fuel);
+        }
+    }
+}
