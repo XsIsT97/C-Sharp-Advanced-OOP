67eb91554d123f20f7023c660d40959311a0b4e3
Galin <galin_3xg@abv.bg>
2024-02-07 18:51:43 +0200

    Add file

---
+++ 01. OOP-Inheritance/Inheritance-Exercises/03. Players and Monsters/DarkWizard.cs
@@ -0,0 +1,19 @@
+﻿using _03._Players_and_Monsters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class DarkWizard : Wizard
+    {
+        public DarkWizard(string username, int level) : base(username, level)
+        {
+            Username = username;
+            Level = level;
+        }
+    }
+}
---
+++ 01. OOP-Inheritance/Inheritance-Exercises/03. Players and Monsters/SoulMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class SoulMaster : DarkWizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        {
+            Username = username;
+            Level = level;
+        }
+    }
+}
