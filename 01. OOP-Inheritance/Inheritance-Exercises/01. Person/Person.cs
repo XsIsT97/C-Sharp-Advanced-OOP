6a6109994289ed85a009ae2c759a18c20eb287b6
Galin <galin_3xg@abv.bg>
2024-02-07 18:50:39 +0200

    Add file

---
+++ 01. OOP-Inheritance/Inheritance-Exercises/01. Person/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        
+        public Person (string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name 
+        {
+            get => name;
+            set => name = value;
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
