9e021878b65401d65d13055b8a6c3d0c83042367
Galin <galin_3xg@abv.bg>
2024-02-08 23:19:34 +0200

    Add file

---
+++ 02. OOP-Encapsulation/Encapsulation- Lab/01. Sort Persons by Name and Age/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persone = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var commandArguments = Console.ReadLine().Split(' ');
+                var person = new Person(commandArguments[0], commandArguments[1], int.Parse(commandArguments[2]));
+                persone.Add(person);
+            }
+
+            persone.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
