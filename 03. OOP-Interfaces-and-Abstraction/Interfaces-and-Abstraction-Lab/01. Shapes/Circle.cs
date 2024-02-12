d5df2187d22e8168bcc40c18ac2e4ba278c5c794
Galin <galin_3xg@abv.bg>
2024-02-12 19:05:19 +0200

    Add file

---
+++ 03. OOP-Interfaces-and-Abstraction/Interfaces-and-Abstraction-Lab/01. Shapes/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int Radius 
+        {
+            get => radius;
+            set => radius = value;
+        }
+
+        public void Draw()
+        {
+            double rIn = radius - 0.4;
+            double rOut = radius + 0.4;
+
+            for (double i = radius; i >= -radius; i--)
+            {
+                for (double j = -radius; j < rOut; j += 0.5)
+                {
+                    double value = i * i + j * j;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
