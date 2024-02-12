eda22eac5faeb4f70b7dfdc844385dff3d717baa
Galin <galin_3xg@abv.bg>
2024-02-12 19:05:43 +0200

    Add file

---
+++ 03. OOP-Interfaces-and-Abstraction/Interfaces-and-Abstraction-Lab/02. Cars/Seat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model 
+        {
+            get => model;
+            set => model = value;
+        }
+
+        public string Color 
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model}";
+        }
+    }
+}
