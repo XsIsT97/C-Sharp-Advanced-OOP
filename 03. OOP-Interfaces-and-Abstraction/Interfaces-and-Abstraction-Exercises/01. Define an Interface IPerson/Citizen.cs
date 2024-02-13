62accd7846b97ea4db68e1f02572817289b44ee1
Galin <galin_3xg@abv.bg>
2024-02-13 21:45:03 +0200

    Add file

---
+++ 03. OOP-Interfaces-and-Abstraction/Interfaces-and-Abstraction-Exercises/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name 
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age 
+        {
+            get => age;
+            set => age = value;
+        }
+    }
+}
