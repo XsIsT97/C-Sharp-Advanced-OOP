5bc998d19041cb00299ca84915096192b5f719ce
Galin <galin_3xg@abv.bg>
2024-02-14 18:49:39 +0200

    Add file

---
+++ 03. OOP-Interfaces-and-Abstraction/Interfaces-and-Abstraction-Exercises/07. Military Elite/Classes/Commando.cs
@@ -0,0 +1,35 @@
+﻿using MilitaryElite.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MilitaryElite.Classes
+{
+    public class Commando : SpecialisedSoldier, ICommando
+    {
+        public Commando(string id, string firstName, string lastName, decimal salary, Corps corps)
+            : base(id, firstName, lastName, salary, corps)
+        {
+            Missions = new List<IMission>();
+        }
+
+        public List<IMission> Missions { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            sb.AppendLine($"Corps: {Corps}");
+            sb.AppendLine("Missions:");
+
+            foreach (var mission in Missions)
+            {
+                sb.AppendLine($"  {mission}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03. OOP-Interfaces-and-Abstraction/Interfaces-and-Abstraction-Exercises/07. Military Elite/Classes/Engineer.cs
@@ -0,0 +1,35 @@
+﻿using MilitaryElite.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryElite.Classes
+{
+    public class Engineer : SpecialisedSoldier, IEngineer
+    {
+        public Engineer(string id, string firstName, string lastName, decimal salary, Corps corps)
+            : base(id, firstName, lastName, salary, corps)
+        {
+            Repairs = new List<IRepair>();
+        }
+
+        public List<IRepair> Repairs { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            sb.AppendLine($"Corps: {Corps}");
+            sb.AppendLine("Repairs:");
+
+            foreach (var item in Repairs)
+            {
+                sb.AppendLine($"  {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
