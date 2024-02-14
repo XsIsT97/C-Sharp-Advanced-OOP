8527319b7179e7624af4d79b2c4d0cc31ad1b841
Galin <galin_3xg@abv.bg>
2024-02-14 18:50:08 +0200

    Add file

---
+++ 03. OOP-Interfaces-and-Abstraction/Interfaces-and-Abstraction-Exercises/09. Explicit Interfaces/IResident.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterfaces
+{
+    public interface IResident
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        string GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
