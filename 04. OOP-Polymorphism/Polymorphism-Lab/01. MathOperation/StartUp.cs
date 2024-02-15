421960ffcb7dd02295143d4ca582bac0b463e455
Galin <galin_3xg@abv.bg>
2024-02-15 21:29:28 +0200

    Add file

---
+++ 04. OOP-Polymorphism/Polymorphism-Lab/01. MathOperation/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathOperation
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperation operations = new MathOperation();
+
+            Console.WriteLine(operations.Add(2, 3));
+            Console.WriteLine(operations.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(operations.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
