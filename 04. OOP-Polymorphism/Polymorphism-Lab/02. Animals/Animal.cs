072f4319c325477dbffbcd3a4d49b4693b313d99
Galin <galin_3xg@abv.bg>
2024-02-15 21:29:49 +0200

    Add file

---
+++ 04. OOP-Polymorphism/Polymorphism-Lab/02. Animals/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouritFood;
+
+        public Animal(string name, string favouritFood)
+        {
+            Name = name;
+            FavouritFood = favouritFood;
+        }
+
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string FavouritFood 
+        {
+            get { return favouritFood; }
+            set {favouritFood = value; }
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favourite food is {FavouritFood}";
+        }
+    }
+}
