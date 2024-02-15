b0fcbed69a1ee1ab2696fce95d7ae722a4dd1e4a
Galin <galin_3xg@abv.bg>
2024-02-15 21:30:11 +0200

    Add file

---
+++ 04. OOP-Polymorphism/Polymorphism-Lab/03. Shapes/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius 
+        {
+            get => radius;
+            private set => radius = value;
+        }
+
+        public override double CalculatePerimeter() // r * 2 * pi
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override double CalculateArea() // pi * r^2
+        {
+            return Math.PI * (Radius * Radius);
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() + $"{this.GetType().Name}";
+        }
+    }
+}
---
+++ 04. OOP-Polymorphism/Polymorphism-Lab/03. Shapes/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            Shape shape = new Rectangle(width, height);
+
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine(shape.CalculatePerimeter());
+            Console.WriteLine(shape.Draw());
+
+            double radius = double.Parse(Console.ReadLine());
+            shape = new Circle(radius);
+
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine(shape.CalculatePerimeter());
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
