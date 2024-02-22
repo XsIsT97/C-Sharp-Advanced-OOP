d99c12225d69550305fa3cd4bf41dab63faa7056
Galin <galin_3xg@abv.bg>
2024-02-22 22:40:43 +0200

    Add file

---
+++ 05. OOP-Exceptions-and-Error-Handling/01. Square Root/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _1._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                double result = GetSqrt(number);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        private static double GetSqrt(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+
+            return Math.Sqrt(number);
+        }
+    }
+}
