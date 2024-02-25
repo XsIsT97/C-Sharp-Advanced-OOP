030390a8fb3f75e4a15a115a406bcd7d94f4c7be
Galin <galin_3xg@abv.bg>
2024-02-26 00:00:11 +0200

    Add file

---
+++ 06. OOP-Reflection-and-Attributes/Reflection-and-Attributes-Lab/06. Code Tracker/Tracker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public Tracker()
+        {
+            
+        }
+
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(t => t.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute authorAttribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {authorAttribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
