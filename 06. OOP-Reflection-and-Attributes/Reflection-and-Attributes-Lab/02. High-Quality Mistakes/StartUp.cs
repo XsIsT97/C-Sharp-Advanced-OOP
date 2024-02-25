6dedab2e58f0050c9fdc47b7cdc9c29f50ef9167
Galin <galin_3xg@abv.bg>
2024-02-25 23:59:13 +0200

    Add file

---
+++ 06. OOP-Reflection-and-Attributes/Reflection-and-Attributes-Lab/02. High-Quality Mistakes/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
